a5501c32f925188b0dd379ba428e76d5c09eae95
Tong W. Ratanapan <tong.worapol@gmail.com>
2016-10-23 20:45:21 +0700

    test: Check if initial entries are loaded with at least one entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -96,6 +96,18 @@ $(function() {
      * Remember, loadFeed() is asynchronous so this test will require
      * the use of Jasmine's beforeEach and asynchronous done() function.
      */
+
+    beforeEach(function(done) {
+      loadFeed(0, function() {
+        done();
+      });
+    });
+
+    it('are loaded, with at least one entry, from the default feed', function(done) {
+      var feed = $('.feed');
+      expect(feed.find('.entry').length).not.toBe(0);
+      done();
+    });
   });
 
   /* TODO: Write a new test suite named "New Feed Selection"
