43559471aea24f2a1a8fdf34000f62ff6d200cfc
Tong W. Ratanapan <tong.worapol@gmail.com>
2016-10-23 16:57:09 +0700

    test: Check if menu changes visibility when its icon is clicked

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -68,12 +68,24 @@ $(function() {
      * the CSS to determine how we're performing the
      * hiding/showing of the menu element.
      */
+    it('is hidden by default', function() {
+      expect($('body').hasClass('menu-hidden')).toBe(true);
+    });
 
     /* TODO: Write a test that ensures the menu changes
      * visibility when the menu icon is clicked. This test
      * should have two expectations: does the menu display when
      * clicked and does it hide when clicked again.
      */
+    it('changes visibility when menu icon is clicked', function() {
+      var body = $('body'),
+          menuIcon = $('.menu-icon-link');
+
+      menuIcon.trigger('click');
+      expect(body.hasClass('menu-hidden')).toBe(false);
+      menuIcon.trigger('click');
+      expect(body.hasClass('menu-hidden')).toBe(true);
+    });
   })
 
   /* TODO: Write a new test suite named "Initial Entries" */
